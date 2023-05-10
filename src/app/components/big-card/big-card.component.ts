8ecc1c2c836710ce0589de3f692889a7d48599ef
thales santos <thalesabsantos@gmail.com>
2023-05-10 13:24:25 -0300

    conteudo-dinamico

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string = ''
+  @Input()
+  cardTitle:string = ''
+  @Input()
+  cardDescription:string = ''
   constructor() { }
 
   ngOnInit(): void {
